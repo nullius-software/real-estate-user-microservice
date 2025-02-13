ef2b29c977675e9f868502a21efe706777c73615
KevOrtega <kevortega.dev@gmail.com>
2025-02-13 19:17:21 -0300

    feat: add eureka client config

--- src/main/kotlin/com/nullius_real_estate/user_microservice/UserMicroserviceApplication.kt
+++ src/main/kotlin/com/nullius_real_estate/user_microservice/UserMicroserviceApplication.kt
@@ -2,6 +2,7 @@ package com.nullius_real_estate.user_microservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
 class UserMicroserviceApplication
